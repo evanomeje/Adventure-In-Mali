aae1b671f6373e84532acd60a88510e524bc5809
omee2720 <evan.omeje@uleth.ca>
2024-04-30 23:33:50 -0600

    Added first js script. Added message to fix transition for quiting game. git push origin main

---
+++ uiScript.js
@@ -0,0 +1,8 @@
+//Timer for the sorry to see you go.
+
+function SorryTimer() {
+    var message= document.querySelector("SorryMessage");
+    setTimeout(SorryTimer, 5500)
+    window.location.href = "https://github.com/evanomeje";
+}
+SorryTimer();
