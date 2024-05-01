e09cb1e601184b58fa8696630499d136ecaca038
omee2720 <evan.omeje@uleth.ca>
2024-05-01 01:27:56 -0600

    update

--- uiScript.js
+++ uiScript.js
@@ -1,8 +1,9 @@
 //Timer for the sorry to see you go.
 
 function SorryTimer() {
-    var message= document.querySelector("SorryMessage");
-    setTimeout(SorryTimer, 5500)
-    window.location.href = "https://github.com/evanomeje";
+    setTimeout(function() {
+        window.location.href = "https://github.com/evanomeje";
+    }, 2000);
 }
+
 SorryTimer();
