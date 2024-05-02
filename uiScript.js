f0f7525b2f523e23740347a7e4f07c978085dabf
omee2720 <evan.omeje@uleth.ca>
2024-05-01 18:02:40 -0600

    Fixed button quit error

--- uiScript.js
+++ uiScript.js
@@ -1,9 +1,4 @@
-//Timer for the sorry to see you go.
-
-function SorryTimer() {
-    setTimeout(function() {
-        window.location.href = "https://github.com/evanomeje";
-    }, 2000);
-}
-
-SorryTimer();
+// Timer function to redirect to GitHub page
+setTimeout(function() {
+    window.location.href = "https://github.com/evanomeje";
+}, 2000); 
