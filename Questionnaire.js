ca631f381eea1b496ac30c150172470fec47819a
omee2720 <evan.omeje@uleth.ca>
2024-05-09 21:46:09 -0600

    Created a working questionnaire class game

--- Questionnaire.js
+++ Questionnaire.js
@@ -1,3 +1,4 @@
+// Question class
 class Question {
     constructor(question, answerKey, options) {
         this.question = question;
@@ -5,64 +6,80 @@ class Question {
         this.options = options;
     }
 
-    getQuestion() {
-        return this.question;
-    }
+    displayQuestion() {
+        document.getElementById("question-box").innerText = this.question;
+        const answerBoxes = document.querySelectorAll(".answer-box");
+        this.options.forEach((option, index) => {
+            answerBoxes[index].innerText = option;
+        });
+    }       
 
-    getAnswerKey() {
-        return this.answerKey;
-    }
-
-    getOptions() {
-        return this.options;
+    checkAnswer(userAnswer) {
+        const answerKey = this.answerKey.toLowerCase();
+        userAnswer = userAnswer.toLowerCase();
+        if (userAnswer === answerKey) {
+            alert("Correct Answer!");
+        } else {
+            alert("Incorrect Answer!");
+        }
     }
 }
 
+// CouncilGriot class
+class CouncilGriot extends Question {
+    constructor() {
+        super(
+            "Which ruler is known for his hajj pilgrimage to Mecca?",
+            "a",
+            ["a) Mansa Musa (correct)", "b) Sundiata Keita", "c) Askia Muhammad"]
+        );
 
-//------------------------DjenneGate Class----------------------------------------//
+        this.questions = [
+            new Question(
+                "Which ruler is known for his hajj pilgrimage to Mecca?",
+                "a",
+                ["a) Mansa Musa (correct)", "b) Sundiata Keita", "c) Askia Muhammad"]
+            ),
+            new Question(
+                "Who is considered the founder of the Mali Empire?",
+                "c",
+                ["a) Mansa Musa", "b) Askia Muhammad", "c) Sundiata Keita (correct)"]
+            ),
+            new Question(
+                "What is the role of a griot?",
+                "a",
+                ["a) To keep stories and traditions alive through storytelling and music. (correct)", "b) To lead religious ceremonies and rituals.", "c) To manage trade relations with neighboring communities."]
+            )
+        ];
+        this.currentQuestionIndex = 0;
+    }
+
+    displayQuestion() {
+        this.questions[this.currentQuestionIndex].displayQuestion();
+    }
+
+    checkAnswer(userAnswer) {
+        this.questions[this.currentQuestionIndex].checkAnswer(userAnswer);
+    }
+
+    nextQuestion() {
+        this.currentQuestionIndex++;
+        if (this.currentQuestionIndex >= this.questions.length) {
+            alert("No more questions!");
+            this.currentQuestionIndex = this.questions.length - 1;
+        }
+        this.displayQuestion();
+    }
+}
+
+// DjenneGate class
 class DjenneGate extends Question {
     constructor() {
-        super("What is the traditional title of the ruler of the Mali Empire?", "a", ["a) Mansa (answer)", "b) Sultan", "c) Pharaoh"]);
-    }
-}
-
-// Create an instance of the DjenneGate class
-const djenneGateQuestion = new DjenneGate();
-
-// Display question
-document.getElementById("question-box").innerText = djenneGateQuestion.getQuestion();
-
-
-//Display options
-const answerbox1 = document.getElementById("answer-box1");
-const answerbox2 = document.getElementById("answer-box2");
-const answerbox3 = document.getElementById("answer-box3");
-
-const options = djenneGateQuestion.getOptions();
-
-for ( let i = 0; i < options.length; i++ ) {
-    const optionElement = document.createElement("div");
-    optionElement.textContent = options[i];
-
-    if (i === 0) {
-        answerbox1.appendChild(optionElement);
-    } else if (i === 1) {
-        answerbox2.appendChild(optionElement);
-    } else if (i === 2) {
-        answerbox3.appendChild(optionElement);
-    }
-
-}
-
-
-// Function to check answer
-function checkAnswer() {
-    const userAnswer = document.getElementById("fname").value;
-    const answerKey = djenneGateQuestion.getAnswerKey();
-    if (userAnswer.toLowerCase() === answerKey) {
-        alert("Correct Answer!");
-    } else {
-        alert("Incorrect Answer!");
+        super(
+            "What is the name of the famous mosque located in Mali?",
+            "c",
+            ["a) Great Mosque of Mecca", "b) Mosque of Al-Mutawakkil", "c) Great Mosque of Djenne (correct)"]
+        );
     }
 }
 
@@ -71,6 +88,20 @@ function resetInput() {
     document.getElementById("fname").value = "";
 }
 
-//******************************************Counicl of griot class*********************************************** *//
+// Function to check the user's answer
+function checkAnswer() {
+    const userAnswer = document.getElementById("fname").value;
+    councilGriot.checkAnswer(userAnswer);
+}
 
+// Function for "Next" button
+function nextQuestion() {
+    councilGriot.nextQuestion();
+}
+
+// Create an instance of the CouncilGriot class
+const councilGriot = new DjenneGate();
+
+// Display initial question for CouncilGriot class
+councilGriot.displayQuestion();
 
