3794164d5eb430ade78068d84af66ec08b16568e
omee2720 <evan.omeje@uleth.ca>
2024-05-11 22:52:32 -0600

    added market challenge:puzzle round

---
+++ MarketChallenge.js
@@ -0,0 +1,47 @@
+class MarketChallenge {
+    constructor(answerKey) {
+        this.answerKey = answerKey.map(item => item.toLowerCase()); 
+        this.collection = [];
+    }
+
+    display() {
+        const collectionText = document.getElementById("collection-text");
+      
+        collectionText.innerHTML = "";
+      
+        this.collection.forEach(item => {
+            const textBox = document.createElement("input");
+            textBox.type = "text";
+            textBox.value = `* ${item}`;
+            textBox.size = 170000000000000000000; 
+            textBox.readOnly = true; 
+            collectionText.appendChild(textBox);
+            collectionText.appendChild(document.createElement("br")); 
+        });
+    }
+
+    checkAnswer(userAnswer) {
+        userAnswer = userAnswer.toLowerCase(); 
+        if (this.answerKey.includes(userAnswer)) {
+            if (!this.collection.includes(userAnswer)) {
+                this.collection.push(userAnswer);
+                this.display();
+            } else {
+                alert("Item already exists in the collection. Try again.");
+            }
+        } else {
+            alert("Incorrect! That's not part of the mask.");
+        }
+    }
+}
+
+const marketChallenge = new MarketChallenge(["Wood", "Clay", "Beads", "Cowrie", "Knife", "Pigment"]);
+
+function checkAnswer() {
+    const userAnswer = document.getElementById("fname").value; 
+    marketChallenge.checkAnswer(userAnswer);
+}
+
+function resetInput() {
+    document.getElementById("fname").value = "";
+}
